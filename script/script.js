b0d37e791385264df4cba60bc884461f19f677df
Angelina Bestemianikova <lina.bst@mail.ru>
2023-12-18 12:34:00 +0300

    footer is added

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(function () {        
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 1000);
+            return false;
+        });
+    });
+});
